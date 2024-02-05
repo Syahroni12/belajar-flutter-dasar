555a52177012c09708732b7d17f1ac1b08122054
Syahroni12 <ronixria@gmail.com>
2024-02-05 14:54:55 +0700

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -5,39 +5,30 @@ main() {
 }
 
 class Myapp extends StatelessWidget {
+  final List<Color> berna = [
+    Colors.red,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.orange
+  ];
+  List<Widget> lisku = List.generate(
+      100,
+      (index) => Text(
+            "${index + 1}",
+            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+          ));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("column"),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View"),
+        ),
+        body: ListView(
+          children: lisku,
+        ),
       ),
-      body: Stack(
-        children: [
-          Container(
-            height: 400,
-            width: 150,
-            color: Colors.lightGreen,
-          ),
-          Container(
-            height: 300,
-            width: 100,
-            color: Color.fromARGB(255, 129, 21, 211),
-          ),
-          Container(
-            height: 200,
-            width: 70,
-            color: Color.fromARGB(255, 17, 32, 3),
-          ),
-          Container(
-            height: 100,
-            width: 50,
-            color: Color.fromARGB(255, 156, 25, 25),
-          ),
-        ],
-      ),
-    ));
+    );
   }
 }
