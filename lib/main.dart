ad713d7745b764296a263e6e74c39c97efb2d88d
Syahroni12 <ronixria@gmail.com>
2024-02-06 21:48:24 +0700

    materi list tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
-  final List<Color> berna = [
-    Colors.red,
-    Colors.yellow,
-    Colors.green,
-    Colors.blue,
-    Colors.orange
-  ];
-  List<Widget> lisku = List.generate(
-      100,
-      (index) => Text(
-            "${index + 1}",
-            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
-          ));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List View"),
+          title: Text("List Tile"),
         ),
         body: ListView(
-          children: lisku,
+          children: [
+            ListTile(
+              title: Text("Muhammad Syahroni"),
+              subtitle: Text("Prodi : Teknik Informatika"),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+              tileColor: Colors.red,
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Muhammad Syahroni"),
+              subtitle: Text("Prodi : Teknik Informatika"),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Muhammad Syahroni"),
+              subtitle: Text("Prodi : Teknik Informatika"),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text("Muhammad Syahroni"),
+              subtitle: Text("Prodi : Teknik Informatika"),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 PM"),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+          ],
         ),
       ),
     );
